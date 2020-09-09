100b1dd988f38dacf42dccca3e7eeb089cdc9323
yyyyyyyan <contact@yyyyyyyan.tech>
2020-09-09 19:46:11 -0300

    Rename cli.py to __main__.py and add screamer attribute to Bandcamper

---
+++ bandcamper/__main__.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentParser
+
+from bandcamper import Bandcamper
+from bandcamper.screamo import Screamer
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Download tracks and albums from Bandcamp")
+    parser.add_argument(
+        "urls",
+        nargs="*",
+        metavar="URL",
+        help="URLs/artists subdomains to download songs",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="files",
+        default=[],
+        action="append",
+        help="Download from URLs/artists subdomains listed on file",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore-errors",
+        action="store_true",
+        help="Don't end the program in case of any (predictable) error; show warning instead",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="count",
+        default=0,
+        help="Run bandcamper with less output. Use this flag 3 times to completely disable any output",
+    )
+    parser.add_argument(
+        "--no-colors",
+        dest="colored",
+        action="store_false",
+        help="Disable colored output",
+    )
+
+    args = parser.parse_args()
+    screamer = Screamer(args.quiet, args.colored, args.ignore_errors)
+    urls = args.urls
+    for filename in args.files:
+        try:
+            with open(filename) as url_list:
+                urls.extend(url_list.read().split())
+        except FileNotFoundError:
+            screamer.error(f"File '{filename}' not found!")
+
+    bandcamper = Bandcamper(screamer, *urls)
+    if not bandcamper.urls:
+        screamer.error(
+            "You must give at least one URL/artist subdomain to download",
+            force_error=True,
+        )
+
+    print(bandcamper.urls)
--- bandcamper/bandcamper.py
+++ bandcamper/bandcamper.py
@@ -11,55 +11,55 @@ class Bandcamper:
     Bandcamper objects are responsible of downloading and organizing
     the music from Bandcamp, among with writing their metadata.
     """
+
     # Bandcamp subdomain and URL Regexes taken from the pick_subdomain step of creating an artist page.
     # Rules for subdomains are:
     #   - At least 4 characters long
     #   - Only lowercase letters, numbers and hyphens are allowed
     #   - Must not end with hyphen
     _BANDCAMP_SUBDOMAIN_PATTERN = r"^[a-z0-9][a-z0-9-]{2,}[a-z0-9]"
-    BANDCAMP_SUBDOMAIN_REGEX = re.compile(_BANDCAMP_SUBDOMAIN_PATTERN, flags=re.IGNORECASE)
-    BANDCAMP_URL_REGEX = re.compile(_BANDCAMP_SUBDOMAIN_PATTERN + r"\.bandcamp\.com$", flags=re.IGNORECASE)
+    BANDCAMP_SUBDOMAIN_REGEX = re.compile(
+        _BANDCAMP_SUBDOMAIN_PATTERN, flags=re.IGNORECASE
+    )
+    BANDCAMP_URL_REGEX = re.compile(
+        _BANDCAMP_SUBDOMAIN_PATTERN + r"\.bandcamp\.com$", flags=re.IGNORECASE
+    )
 
     # Bandcamp IP for custom domains taken from the article "How do I set up a custom domain on Bandcamp?".
     # Article available on:
     #   https://get.bandcamp.help/hc/en-us/articles/360007902973-How-do-I-set-up-a-custom-domain-on-Bandcamp-
     CUSTOM_DOMAIN_IP = "35.241.62.186"
 
-    def __init__(self, *names, **kwargs):
+    def __init__(self, screamer, *urls, **kwargs):
         self.params = {
             "preferred_scheme": "https",
-            "proxies": {
-                "http": getenv("HTTP_PROXY"),
-                "https": getenv("HTTPS_PROXY")
-            },
+            "proxies": {"http": getenv("HTTP_PROXY"), "https": getenv("HTTPS_PROXY")},
             "ignore_errors": False,
         }
+        self.screamer = screamer
         self.params.update(deepcopy(kwargs))
         self.urls = set()
-        for name in names:
-            url = self._get_url(name)
-            if name:
+        for url in urls:
+            url = self._get_url(url)
+            if url:
                 self.urls.add(url)
 
     def _get_url(self, name):
-        url = ""
+        url = None
         parsed_url = urlparse(name)
         if parsed_url.scheme and parsed_url.netloc:
             domain = parsed_url.netloc
             # TODO: Consider changing gethostbyname by something that supports proxies.
-            if self.BANDCAMP_URL_REGEX.match(domain) or gethostbyname(domain) == self.CUSTOM_DOMAIN_IP:
+            if (
+                self.BANDCAMP_URL_REGEX.match(domain)
+                or gethostbyname(domain) == self.CUSTOM_DOMAIN_IP
+            ):
                 scheme = self.params.get("preferred_scheme") or parsed_url.scheme
                 url = urljoin(f"{scheme}://{domain.lower()}", parsed_url.path)
-            elif self.params.get("ignore_errors"):
-                # TODO: Add warning.
-                pass
             else:
-                raise ValueError(f"{name} is not a valid Bandcamp URL")
+                self.screamer.error(f"{name} is not a valid Bandcamp URL")
         elif self.BANDCAMP_SUBDOMAIN_REGEX.match(name):
             url = f"https://{name.lower()}.bandcamp.com"
-        elif self.params.get("ignore_errors"):
-            # TODO: Add warning.
-            pass
         else:
-            raise ValueError(f"{name} is not a valid Bandcamp subdomain")
+            self.screamer.error(f"{name} is not a valid Bandcamp subdomain")
         return url
