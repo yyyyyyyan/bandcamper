4f843a44802e77c961f1da629c146ad8408aeba5
yyyyyyyan <contact@yyyyyyyan.tech>
2020-09-13 19:36:39 -0300

    Adjust some arguments in __main__.py.

--- bandcamper/__main__.py
+++ bandcamper/__main__.py
@@ -9,34 +9,35 @@ if __name__ == "__main__":
         "urls",
         nargs="*",
         metavar="URL",
-        help="URLs/artists subdomains to download songs",
+        help="URLs/artists subdomains to download songs from.",
     )
     parser.add_argument(
         "-f",
         "--file",
         dest="files",
+        metavar="<file>",
         default=[],
         action="append",
-        help="Download from URLs/artists subdomains listed on file",
+        help="Download from URLs/artists subdomains listed on file. This option can be used multiple times.",
     )
     parser.add_argument(
         "-i",
         "--ignore-errors",
         action="store_true",
-        help="Don't end the program in case of any (predictable) error; show warning instead",
+        help="Don't end the program in case of any (predictable) error; show warning instead.",
     )
     parser.add_argument(
         "-q",
         "--quiet",
         action="count",
         default=0,
-        help="Run bandcamper with less output. Use this flag 3 times to completely disable any output",
+        help="Run bandcamper with less output. Use this flag 3 times to completely disable any output.",
     )
     parser.add_argument(
         "--no-colors",
         dest="colored",
         action="store_false",
-        help="Disable colored output",
+        help="Disable colored output.",
     )
 
     args = parser.parse_args()
