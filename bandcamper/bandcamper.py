94d290887cbe6c5ace381b31c0bc3509008561d2
yyyyyyyan <contact@yyyyyyyan.tech>
2020-09-13 21:02:51 -0300

    Replace socket.gethostbyname for simple streamed request, in order to preserve proxy settings. Improve handling of URLs.

--- bandcamper/bandcamper.py
+++ bandcamper/bandcamper.py
@@ -1,12 +1,13 @@
 import re
-from copy import deepcopy
 from os import getenv
-from socket import gethostbyname
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urlparse
+
+import requests
+from requests.exceptions import RequestException
 
 
 class Bandcamper:
-    """Bandcamper class.
+    """Represents .
 
     Bandcamper objects are responsible of downloading and organizing
     the music from Bandcamp, among with writing their metadata.
@@ -17,12 +18,13 @@ class Bandcamper:
     #   - At least 4 characters long
     #   - Only lowercase letters, numbers and hyphens are allowed
     #   - Must not end with hyphen
-    _BANDCAMP_SUBDOMAIN_PATTERN = r"^[a-z0-9][a-z0-9-]{2,}[a-z0-9]"
+    _BANDCAMP_SUBDOMAIN_PATTERN = r"[a-z0-9][a-z0-9-]{2,}[a-z0-9]"
     BANDCAMP_SUBDOMAIN_REGEX = re.compile(
         _BANDCAMP_SUBDOMAIN_PATTERN, flags=re.IGNORECASE
     )
     BANDCAMP_URL_REGEX = re.compile(
-        _BANDCAMP_SUBDOMAIN_PATTERN + r"\.bandcamp\.com$", flags=re.IGNORECASE
+        r"(?:www\.)?" + _BANDCAMP_SUBDOMAIN_PATTERN + r"\.bandcamp\.com",
+        flags=re.IGNORECASE,
     )
 
     # Bandcamp IP for custom domains taken from the article "How do I set up a custom domain on Bandcamp?".
@@ -32,34 +34,44 @@ class Bandcamper:
 
     def __init__(self, screamer, *urls, **kwargs):
         self.params = {
-            "preferred_scheme": "https",
+            "force_https": True,
             "proxies": {"http": getenv("HTTP_PROXY"), "https": getenv("HTTPS_PROXY")},
             "ignore_errors": False,
         }
         self.screamer = screamer
-        self.params.update(deepcopy(kwargs))
+        self.params.update(kwargs)
         self.urls = set()
         for url in urls:
             url = self._get_url(url)
             if url:
                 self.urls.add(url)
 
-    def _get_url(self, name):
-        url = None
-        parsed_url = urlparse(name)
-        if parsed_url.scheme and parsed_url.netloc:
-            domain = parsed_url.netloc
-            # TODO: Consider changing gethostbyname by something that supports proxies.
-            if (
-                self.BANDCAMP_URL_REGEX.match(domain)
-                or gethostbyname(domain) == self.CUSTOM_DOMAIN_IP
-            ):
-                scheme = self.params.get("preferred_scheme") or parsed_url.scheme
-                url = urljoin(f"{scheme}://{domain.lower()}", parsed_url.path)
-            else:
-                self.screamer.error(f"{name} is not a valid Bandcamp URL")
-        elif self.BANDCAMP_SUBDOMAIN_REGEX.match(name):
-            url = f"https://{name.lower()}.bandcamp.com"
+    def _is_valid_custom_domain(self, url):
+        valid = False
+        try:
+            res = requests.get(url, stream=True, proxies=self.params.get("proxies"))
+        except RequestException:
+            self.screamer.error(f"Unable to connect to {url}!")
         else:
-            self.screamer.error(f"{name} is not a valid Bandcamp subdomain")
-        return url
+            valid = res.raw._connection.sock.getpeername()[0] == self.CUSTOM_DOMAIN_IP
+        finally:
+            return valid
+
+    def _get_url(self, name):
+        valid_url = ""
+        if self.BANDCAMP_SUBDOMAIN_REGEX.fullmatch(name):
+            valid_url = f"https://{name.lower()}.bandcamp.com"
+        else:
+            parsed_url = urlparse(name)
+            if not parsed_url.scheme:
+                parsed_url = urlparse("https://" + name)
+            elif self.params.get("force_https"):
+                parsed_url = parsed_url._replace(scheme="https")
+            url = parsed_url.geturl()
+            if self.BANDCAMP_URL_REGEX.match(
+                parsed_url.netloc
+            ) or self._is_valid_custom_domain(url):
+                valid_url = url
+            else:
+                self.screamer.error(f"{name} is not a valid Bandcamp URL or subdomain")
+        return valid_url
