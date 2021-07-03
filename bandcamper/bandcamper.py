091893c4b7381de7d5a8e7eb685d05996ec01df2
yyyyyyyan <contact@yyyyyyyan.tech>
2021-07-03 19:08:22 -0300

    Remove screamer from Bandcamper object.

--- bandcamper/bandcamper.py
+++ bandcamper/bandcamper.py
@@ -7,7 +7,6 @@ from urllib.parse import urlparse
 import click
 import requests
 from bs4 import BeautifulSoup
-from requests.exceptions import RequestException
 
 from bandcamper.requests_utils import get_download_file_extension
 from bandcamper.requests_utils import get_random_user_agent
@@ -63,7 +62,6 @@ class Bandcamper:
         self,
         base_path,
         urls,
-        screamer,
         http_proxy=None,
         https_proxy=None,
         force_https=True,
@@ -73,7 +71,6 @@ class Bandcamper:
         self.urls = {*urls}
         for url in urls:
             self.add_url(url)
-        self.screamer = screamer
         self.proxies = {"http": http_proxy, "https": https_proxy}
         self.force_https = True
         self.headers = {"User-Agent": get_random_user_agent()}
@@ -81,42 +78,28 @@ class Bandcamper:
     def _get_request_or_error(self, url, **kwargs):
         proxies = kwargs.pop("proxies", self.proxies)
         headers = kwargs.pop("headers", self.headers)
-        try:
-            response = requests.get(url, proxies=proxies, headers=headers, **kwargs)
-            response.raise_for_status()
-        except RequestException as err:
-            self.screamer.error(str(err))
-            return None
+        response = requests.get(url, proxies=proxies, headers=headers, **kwargs)
+        response.raise_for_status()
         return response
 
     def _is_valid_custom_domain(self, url):
-        valid = False
         response = self._get_request_or_error(url, stream=True)
-        if response is not None:
-            valid = (
-                response.raw._connection.sock.getpeername()[0] == self.CUSTOM_DOMAIN_IP
-            )
-        return valid
+        return response.raw._connection.sock.getpeername()[0] == self.CUSTOM_DOMAIN_IP
 
     def _add_urls_from_artist(self, source_url):
-        self.screamer.info(f"Scraping URLs from {source_url}")
-        url_count = 0
         response = self._get_request_or_error(source_url)
-        if response is not None:
-            base_url = "https://" + urlparse(source_url).netloc.strip("/ ")
-            soup = BeautifulSoup(response.content, "lxml")
-            for a in soup.find("ol", id="music-grid").find_all("a"):
-                parsed_url = urlparse(a.get("href"))
-                if parsed_url.scheme:
-                    url = urljoin(
-                        f"{parsed_url.scheme}://" + parsed_url.netloc.strip("/ "),
-                        parsed_url.path.strip("/ "),
-                    )
-                else:
-                    url = urljoin(base_url, parsed_url.path.strip("/ "))
-                self.urls.add(url)
-                url_count += 1
-        self.screamer.info(f"Found {url_count} URLs in {source_url}")
+        base_url = "https://" + urlparse(source_url).netloc.strip("/ ")
+        soup = BeautifulSoup(response.content, "lxml")
+        for a in soup.find("ol", id="music-grid").find_all("a"):
+            parsed_url = urlparse(a.get("href"))
+            if parsed_url.scheme:
+                url = urljoin(
+                    f"{parsed_url.scheme}://" + parsed_url.netloc.strip("/ "),
+                    parsed_url.path.strip("/ "),
+                )
+            else:
+                url = urljoin(base_url, parsed_url.path.strip("/ "))
+            self.urls.add(url)
 
     def add_url(self, name):
         if self.BANDCAMP_SUBDOMAIN_REGEX.fullmatch(name):
@@ -138,41 +121,31 @@ class Bandcamper:
                 else:
                     self.urls.add(url)
             else:
-                self.screamer.error(f"{name} is not a valid Bandcamp URL or subdomain")
+                raise ValueError(f"{name} is not a valid Bandcamp URL or subdomain")
 
     def _get_music_data(self, url):
         response = self._get_request_or_error(url)
-        if response is None:
-            return None
         soup = BeautifulSoup(response.content, "lxml")
         data = json.loads(soup.find("script", {"data-tralbum": True})["data-tralbum"])
         data["art_url"] = soup.select_one("div#tralbumArt > a.popupImage")["href"]
         return data
 
     def download_to_file(self, url, file_path):
-        try:
-            with requests.get(
-                url, stream=True, proxies=self.proxies, headers=self.headers
-            ) as response:
-                response.raise_for_status()
-                file_ext = get_download_file_extension(
-                    response.headers.get("Content-Type")
-                )
-                file_path = self.base_path / str(file_path).format(ext=file_ext)
-                # TODO: I need to somehow change this Progress Bar to Screamer
-                with file_path.open("wb") as file:
-                    with click.progressbar(
-                        response.iter_content(chunk_size=1024),
-                        length=response.headers.get("Content-Length"),
-                        label=file_path.name,
-                    ) as bar:
-                        for chunk in bar:
-                            file.write(chunk)
-        except RequestException as err:
-            self.screamer.error(str(err), True)
-        else:
-            self.screamer.success(f"Downloaded {file_path}")
-            return file_path
+        with requests.get(
+            url, stream=True, proxies=self.proxies, headers=self.headers
+        ) as response:
+            response.raise_for_status()
+            file_ext = get_download_file_extension(response.headers.get("Content-Type"))
+            file_path = self.base_path / str(file_path).format(ext=file_ext)
+            with file_path.open("wb") as file:
+                with click.progressbar(
+                    response.iter_content(chunk_size=1024),
+                    length=response.headers.get("Content-Length"),
+                    label=file_path.name,
+                ) as bar:
+                    for chunk in bar:
+                        file.write(chunk)
+        return file_path
 
     def _free_download(self, url, download_formats):
         response = self._get_request_or_error(url)
@@ -181,7 +154,6 @@ class Bandcamper:
         downloadable = download_data["download_items"][0]["downloads"]
         for fmt in download_formats:
             if fmt in downloadable:
-                self.screamer.info(f"Downloading {fmt}...", True)
                 parsed_url = urlparse(downloadable[fmt]["url"])
                 stat_path = parsed_url.path.replace("/download/", "/statdownload/")
                 fwd_url = parsed_url._replace(path=stat_path).geturl()
@@ -195,24 +167,19 @@ class Bandcamper:
                 elif fwd_data["result"].lower() == "err":
                     download_url = fwd_data["retry_url"]
                 else:
-                    self.screamer.error(f"Error downloading {fmt} from {fwd_url}")
-                    continue
+                    raise ValueError(f"Error downloading {fmt} from {fwd_url}")
                 print(download_url)
                 # self.download_to_file(download
             else:
-                self.screamer.warning(f"{fmt} download not found", True)
+                raise ValueError(f"{fmt} download not found", True)
 
     def download_all(self, download_formats):
         download_formats = set(download_formats)
 
-        self.screamer.info(f"Starting download of {len(self.urls)} items...")
         for url in self.urls:
-            self.screamer.info(f"Getting music data from {url}")
             music_data = self._get_music_data(url)
             if music_data is None:
-                self.screamer.error(f"Failed to get music data from {url}", True)
-                continue
+                raise ValueError(f"Failed to get music data from {url}", True)
 
             if music_data.get("freeDownloadPage"):
-                self.screamer.success("Free download link available", True)
                 self._free_download(music_data["freeDownloadPage"], download_formats)
